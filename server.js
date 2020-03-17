64a534eec234853a7f4bb4bb8c0e304ed005b0c2
Stefano <stefan.emailacct@gmail.com>
2020-03-17 17:33:12 +0000

    localhost-secured-https

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
 
 //express instance
 const app = express();
 
-//load filesystem 
-const fs = require("fs");
+
+const certOptions = {
+  key: fs.readFileSync(path.resolve('local-cert/server.key')),
+  cert: fs.readFileSync(path.resolve('local-cert/server.crt'))
+}
 
 //handling JSON data
 app.use(bodyParser.json());
@@ -17,6 +23,12 @@ const routes = require("./routes/routes.js")(app, fs);
 
 
 //server port 5000
-const server = app.listen(5000, () => {
-  console.log("\n Listening on port %s...", server.address().port);
-});
+// const server = app.listen(5000, () => {
+//   console.log("\n Listening on port %s...", server.address().port);
+// });
+
+
+
+const server = https.createServer(certOptions,app)
+
+server.listen(5000);
